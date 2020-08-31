768dceedcb6a9702c19bebbeefb178fe20ed2b29
sergey_nacc <sergeynaksin@gmail.com>
2020-08-31 20:36:53 +0700

    Transformation variables and add getOperation

--- src/calc/Calculator.java
+++ src/calc/Calculator.java
@@ -1,43 +1,46 @@
 package calc;
 
-import com.sun.javafx.collections.VetoableListDecorator;
-
 import java.util.Scanner;
 
 public class Calculator {
 
-    Scanner scanner = new Scanner(System.in);
+    static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        int num1 = getInt();
-        int num2 = getInt();
-        char operation;
-        int result;
+        float num1 = getFloat();
+        float num2 = getFloat();
+        char operation = getOperation();
+        float result;
         System.out.println("Ответ: " + result);
     }
 
-    public static int getInt() {
-        System.out.print("Введите первое целое число: ");
-        int num;
+    public static float getFloat() {
+        System.out.print("Введите первое число: ");
+        float num;
         if (scanner.hasNext()) {
-            num = scanner.nextInt();
+            num = scanner.nextFloat();
         } else {
-            System.out.println("Вы ввели не целое цисло!");
-            num = getInt();
+            System.out.print("Вы ввели не число!");
+            num = getFloat();
         }
         return num;
     }
 
-
+    public static char getOperation() {
         System.out.print("Введите действие из предложенных(+, -, * или /): ");
+        char operation;
         if (scanner.hasNext()) {
             operation = scanner.next().charAt(0);
         } else {
             System.out.print("Выбрали не то действие!");
+            operation = getOperation();
         }
+        return operation;
+    }
 
-        System.out.print("Введите второе число: ");
-        num2 = scanner.nextInt();
+
+
+
 
         switch () {
             case "+":
